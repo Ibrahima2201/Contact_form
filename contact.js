de9d83412f0ff6a2d7259010a522098cf0b69715
https://github.com/Ibsoncouldy/CSC-380.git <iboucouldy95@gmail.com>
2022-01-18 00:42:06 -0500

    .

---
+++ contact.js
@@ -0,0 +1,25 @@
+$(document).ready(function (){
+    function init(){
+        //section to input the name
+        if(localStorage["name"]){
+            $('#name').val(localStorage["name"]);
+        }
+    //section for the email
+    if(localStorage["email"]){
+        $('#email').val(localStorage["email"]);
+    }
+    //section for the message
+    if(localStorage["message"]){
+        $('#message').val(localStorage["message"]);
+    }
+}
+    init();
+
+
+});
+//Save the data in the local storage.
+
+$('stored').change(function(){
+    localStorage[$(this).attr('name')] = $(this).val();
+
+});
